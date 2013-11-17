6c4f8acfb2fb35708082f7a1871fe216da474100
Greg Pascale <greg@clipboard.com>
2013-11-17 11:30:47 -0800

    First commit. Grunt stuff

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+app.use(express.logger());
+var fs = require('fs');
+
+app.get(['/', '/index.html'], function(req, res) {
+	fs.readFile(__dirname + '/public/html/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Chordgrids listening on " + port);
+});
