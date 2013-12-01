8bf8552f7a6c1439040f312fca1fd7f24d904e14
Greg Pascale <gpascale@salesforce.com>
2013-12-01 04:24:54 -0500

    Temporarily check in public folder for heroku deployment

---
+++ public/js/models/ChordGridCollection.js
@@ -0,0 +1,50 @@
+;(function() {
+    var app = window.ChordGrids = (window.ChordGrids || { });
+
+    app.ChordGridCollection = Backbone.Collection.extend({
+        model: app.ChordGrid,
+        sync: function(method, model, options) {
+            switch(method) {
+                case 'read':
+                    console.log('read ' + model);
+                    break;
+                case 'create':
+                case 'update':
+                    var str = '';
+                    _.each(this.models, function(model) {
+                        var data = model.get('data');
+                        _.each(data, function(row) {
+                            str += row.join('');
+                        });
+                    });
+                    console.log(str);
+                    break;
+            }
+            if (options && options.success)
+                options.success('SHUCKSHESH!');
+        },
+
+        save: function() {
+            var res = [ ];
+            _.each(this.models, function(model) {
+                res.push(model.toJSON());
+            });
+            console.log(JSON.stringify(res));
+        },
+
+        load: function(str) {
+            var m = [ ];
+            var json = JSON.parse(str);
+            for (var i = 0; i < json.length; ++i) {
+                var data = json[i];
+                var model = new app.ChordGrid({
+                    name: data.name,
+                    fret: data.fret,
+                    data: data.data
+                });
+                m.push(model);
+            }
+            this.set(m);
+        }
+    });
+}());
