7fb63ead49367a83672ef4f69a7768141c1d8a7d
Greg Pascale <greg@clipboard.com>
2013-12-08 01:26:06 -0800

    Add some guitar sounds. Better timing in playback

--- src/client/js/playback.js
+++ src/client/js/playback.js
@@ -10,11 +10,16 @@ app.playback = {
 			return;
 		}
 
+		var instrumentName = "acoustic_guitar_nylon";
+
 		var self = this;
 		MIDI.loadPlugin({
 	                soundfontUrl: "ext/static/soundfont/",
-	                instrument: "acoustic_grand_piano",
+	                instrument: instrumentName,
 	                callback: function() {
+	                	var instNumber = MIDI.GeneralMIDI.byName[instrumentName].number;
+	                	for (var i = 0; i < 6; ++i)
+	                		MIDI.programChange(i, instNumber);
 	                	self._initialized = true;
 	                    callback && callback();
 	                }
@@ -24,12 +29,20 @@ app.playback = {
 	play: function(chordGrid) {
 		var data = chordGrid.get('data');
 		var capo = chordGrid.get('fret') - 1;
+
+		var maxSymbol = 0;
+		for (var string = 0; string < 6; ++string)
+			for (var fret = 0; fret < 6; ++fret)
+				if (data[fret][string] > maxSymbol)
+					maxSymbol = data[fret][string];
+		var mult = 1 / maxSymbol;
+
 		for (var string = 0; string < 6; ++string) {
 			for (var fret = 0; fret < 6; ++fret) {
 				if (data[fret][string]) {
 					var note = this._getNote(capo + fret, string);
 					MIDI.setVolume(string, 127);
-					MIDI.noteOn(string, note, 127, 0.25 * (data[fret][string] - 1));
+					MIDI.noteOn(string, note, 127, mult * (data[fret][string] - 1));
 					MIDI.noteOff(string, note, .9);
 				}
 			}
